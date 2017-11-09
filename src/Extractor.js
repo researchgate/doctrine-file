8a2902eee4c39b0f7228e7ab29d17b400a650e06
Daniel Tschinder <daniel.tschinder@researchgate.net>
2017-11-09 23:33:31 +0100

    chore(lint): Use rg config

--- src/Extractor.js
+++ src/Extractor.js
@@ -18,49 +18,49 @@ export default class Extractor extends Transform {
     this.opts = opts;
   }
 
-  _resetChunk(): void {
+  resetChunk(): void {
     this.unfinishedChunk = [];
   }
 
-  _addLine(line: string, reset: boolean = false): void {
-    if (reset) this._resetChunk();
+  addLine(line: string, reset: boolean = false): void {
+    if (reset) this.resetChunk();
     this.unfinishedChunk.push(line);
   }
 
-  _getRawCommentAndReset(): string {
+  getRawCommentAndReset(): string {
     const comment = this.unfinishedChunk.join('\n');
-    this._resetChunk();
+    this.resetChunk();
 
     return comment;
   }
 
-  _consumeLine(line: string): ?CommentObject {
+  consumeLine(line: string): ?CommentObject {
     const match = line.match(jsdoc.singleLine);
     if (match) {
       // singleline
-      return this._addDoc(match[1].trim());
+      return this.addDoc(match[1].trim());
     } else if (line.match(jsdoc.start)) {
       // start multiline
-      this._addLine(line, true);
+      this.addLine(line, true);
     } else if (this.unfinishedChunk.length) {
       if (line.match(jsdoc.end)) {
         // end multiline
-        this._addLine(line);
+        this.addLine(line);
 
-        return this._addDoc(this._getRawCommentAndReset());
+        return this.addDoc(this.getRawCommentAndReset());
       } else if (line.match(jsdoc.line)) {
         // line multiline
-        this._addLine(line);
+        this.addLine(line);
       } else {
         // invalid line inbetween jsdoc
-        this._resetChunk();
+        this.resetChunk();
       }
     }
 
     return null;
   }
 
-  _addDoc(docBlock: string): CommentObject {
+  addDoc(docBlock: string): CommentObject {
     const comment = parse(docBlock, { ...this.opts, unwrap: true });
     // $FlowIssue This is correct as objectMode === true
     this.push(comment);
@@ -72,7 +72,7 @@ export default class Extractor extends Transform {
     const lines = chunk.toString().split(/\r?\n/);
 
     while (lines.length) {
-      this._consumeLine(lines.shift());
+      this.consumeLine(lines.shift());
     }
 
     callback();
@@ -83,7 +83,7 @@ export default class Extractor extends Transform {
     const lines = content.toString().split(/\r?\n/);
 
     while (lines.length) {
-      const comment = this._consumeLine(lines.shift());
+      const comment = this.consumeLine(lines.shift());
       if (comment) comments.push(comment);
     }
 
